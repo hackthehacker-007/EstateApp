6997d1039dc9aacb37dd3b16ecb1c147c3102338
mk880 <mk880@APEX>
2024-09-14 11:44:07 +0530

    backend setup

--- Api/app.js
+++ Api/app.js
@@ -1,6 +1,11 @@
 import express from "express";
+import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
+import userRoute from "./routes/user.route.js";
 
 const app = express();
+
+app.use("/api/auth", authRoute);
 
 app.listen(8000, () => {
   console.log("server is running!");
